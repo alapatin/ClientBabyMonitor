4fa4ce5b5ae6dfb013d728c0a970ec66bee961be
Aleksandr Lapatin <a.lapatin@icloud.com>
2019-01-01 20:16:09 +0300

    Client Pass SampleBuffer from UInt8

---
+++ CoreDataSample/Client.swift
@@ -0,0 +1,199 @@
+//
+//  Client.swift
+//  CoreDataSample
+//
+//  Created by macbook air on 30/12/2018.
+//  Copyright © 2018 a.lapatin@icloud.com. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class TCPClient: NSObject {
+    
+    let netServiceType = "_babywatcher._tcp."
+    
+    var serviceBrowser: NetServiceBrowser!
+    var services: [NetService] = []
+    var servicesCallback: (([NetService]) -> Void)?
+    
+    var streamsConnected = false
+    var streamsConnectedCallback: (() -> Void)?
+    
+    let serviceDomain = "local"
+    var inputStream: InputStream?
+    var outputStream: OutputStream?
+    var openedStreams = 0
+    
+    static let shared = TCPClient()
+    
+    private override init() {
+        super.init()
+        
+        self.startBrowsingServices()
+    }
+    
+    func startBrowsingServices() {
+        self.serviceBrowser = NetServiceBrowser()
+        self.serviceBrowser?.includesPeerToPeer = true
+        self.serviceBrowser?.delegate = self
+        self.serviceBrowser?.searchForServices(ofType: netServiceType, inDomain: self.serviceDomain)
+    }
+    
+    func connectTo(service: NetService) {
+        
+        var inputStream: InputStream?
+        var outputStream: OutputStream?
+        
+        let success = service.getInputStream(&inputStream, outputStream: &outputStream)
+        
+        if !success {
+            return print("could not connect to service")
+        }
+        self.inputStream  = inputStream
+        self.outputStream = outputStream
+        
+        self.openStreams()
+        
+        print("connecting...")
+    }
+    
+    func openStreams() {
+        guard self.openedStreams == 0 else {
+            return print("streams already opened... \(self.openedStreams)")
+        }
+        
+        
+        self.inputStream?.schedule(in: .current, forMode: .default)
+        self.inputStream?.open()
+        
+        
+        self.outputStream?.schedule(in: .current, forMode: .default)
+        self.outputStream?.open()
+        
+        //        let data = "Hello".data(using: .utf8)!
+        //        var bytesWritten = data.withUnsafeBytes { self.outputStream?.write($0, maxLength: data.count) }
+        
+        print("Open streams")
+        
+        
+    }
+    
+    func closeStreams() {
+        self.inputStream?.remove(from: .current, forMode: .default)
+        self.inputStream?.close()
+        self.inputStream = nil
+        
+        self.outputStream?.remove(from: .current, forMode: .default)
+        self.outputStream?.close()
+        self.outputStream = nil
+        
+        self.streamsConnected = false
+        self.openedStreams = 0
+    }
+    
+    func send() {
+        //        guard self.openedStreams == 2 else {
+        //            return print("no open streams \(self.openedStreams)")
+        //        }
+        //
+        //        guard self.outputStream!.hasSpaceAvailable else {
+        //            return print("no space available")
+        //        }
+        
+        let data = "Hello, World".data(using: .utf8)!
+        
+        let bytesWritten = data.withUnsafeBytes { self.outputStream?.write($0, maxLength: data.count) }
+        
+        //        guard bytesWritten == data.count else {
+        //            self.closeStreams()
+        //            print("something is wrong...")
+        //            return
+        //        }
+        //        print("data written... \(message)")
+    }
+    
+    
+    func sendVideoFrames (frame: CMSampleBuffer){
+        
+        
+        let bufferData = bufferToUInt(sampleBuffer: frame)
+//        print(bufferData)
+        self.outputStream?.write(bufferData, maxLength: bufferData.count)
+        
+//        let pointer = frame!.pointee
+//        let str = String(cString: pointer!)
+//        let data = str.data(using: .utf8)!
+//        let bytesWritten = data.withUnsafeBytes { self.outputStream?.write($0, maxLength: data.count) }
+    }
+    
+    private func bufferToUInt(sampleBuffer: CMSampleBuffer) -> [UInt8] {
+        let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let byterPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        let srcBuff = CVPixelBufferGetBaseAddress(imageBuffer)
+        
+        let data = NSData(bytes: srcBuff, length: byterPerRow * height)
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+//        return [UInt8].init(repeating: 0, count: data.length / MemoryLayout<UInt8>.size)
+        return [UInt8](data as Data)
+    }
+}
+    
+extension TCPClient: NetServiceBrowserDelegate {
+    
+    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        print("service found:" + service.name)
+        self.services.append(service)
+        
+        if !moreComing {
+            if let service = self.services.first {
+                self.connectTo(service: service)
+            }
+        }
+    }
+    
+    func netServiceBrowser(_ browser: NetServiceBrowser, didRemove service: NetService, moreComing: Bool) {
+        print("service removed:" + service.name)
+        self.services = self.services.filter() { $0 != service }
+        
+        if !moreComing {
+            if let service = self.services.first {
+                self.connectTo(service: service)
+            }
+        }
+    }
+}
+
+//extension TCPClient: StreamDelegate {
+//
+//    func stream(aStream: Stream, handleEvent eventCode: Stream.Event) {
+//        if eventCode.contains(.openCompleted) {
+//            self.openedStreams += 1
+//            print("Opened streams: \(openedStreams)")
+//
+//        }
+//        if eventCode.contains(.hasSpaceAvailable) {
+//            if self.openedStreams == 2 && !self.streamsConnected {
+//                print("streams connected.")
+//                self.streamsConnected = true
+//                self.streamsConnectedCallback?()
+//            }
+//        }
+//    }
+//}
+//
+//extension TCPClient: NetServiceDelegate {
+//
+//    func netServiceDidResolveAddress(_ sender: NetService) {
+//        print("Did resolved addres")
+//    }
+//
+//    func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
+//        print("Didn't resolved")
+//    }
+//
+//}
--- CoreDataSample/ViewController.swift
+++ CoreDataSample/ViewController.swift
@@ -7,14 +7,139 @@
 //
 
 import UIKit
+import AVFoundation
+import VideoToolbox
+
 
 class ViewController: UIViewController {
-
+    
+    var captureSession: AVCaptureSession!
+//    var compressionSession: VTCompressionSession!
+    var discoverySession: AVCaptureDevice.DiscoverySession!
+    //    var captureDevice: AVCaptureDevice?
+    var cameraInput: AVCaptureDeviceInput?
+    
+    
+    
+    var t = 0
+    var i = 0
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        TCPClient.shared
+        
+        // Find and get Camera
+        
+        discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes:
+            [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera],
+                                                            mediaType: .video, position: .unspecified)
+        func bestDevice(in position: AVCaptureDevice.Position) -> AVCaptureDevice {
+            let devices = discoverySession.devices
+            guard !devices.isEmpty else { fatalError("Missing capture devices.")}
+            return devices.first(where: { device in device.position == position })!
+        }
+        
+        let captureDevice = bestDevice(in: .back)
+        
+        
+        // Create Session
+        
+        captureSession = AVCaptureSession()
+        
+        // Add Camera to Input
+        
+        do {
+            cameraInput = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        // Add Input to Session
+        
+        if captureSession.canAddInput(cameraInput!){
+            captureSession.addInput(cameraInput!)
+        } else {
+            fatalError("Can't add input to the session")
+        }
+        
+        // Add Output to Session
+        
+        let sessionOutput = AVCaptureVideoDataOutput()
+        sessionOutput.alwaysDiscardsLateVideoFrames = true
+        let queue = DispatchQueue.main
+        sessionOutput.setSampleBufferDelegate(self, queue: queue)
+        guard captureSession.canAddOutput(sessionOutput) else {fatalError()}
+        
+        captureSession.addOutput(sessionOutput)
+        
+        // Add Compression Session
+        
+//        var pointerCompression: UnsafeMutablePointer<VTCompressionSession?>
+//        let statusCompressionSession = VTCompressionSessionCreate(allocator: nil,
+//                                                             width: 200,
+//                                                             height: 320,
+//                                                             codecType: kCMVideoCodecType_H264,
+//                                                             encoderSpecification: nil,
+//                                                             imageBufferAttributes: nil,
+//                                                             compressedDataAllocator: nil,
+//                                                             outputCallback: nil,
+//                                                             refcon: nil,
+//                                                             compressionSessionOut: pointerCompression)
+//        self.compressionSession = pointerCompression.pointee!
+        
+        
+        
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        
+        captureSession.startRunning()
     }
+    
+    @IBAction func send(_ sender: UIButton) {
+        TCPClient.shared.send()
+    }
+        
+}
+//    @IBAction func connect(_ sender: UIButton) {
+//        TCPClient.shared.connectTo()
+//    }
 
-
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        self.t += 1
+        print(t)
+        
+        if CMSampleBufferDataIsReady(sampleBuffer) {
+            self.i += 1
+            print(self.i)
+            
+            TCPClient.shared.sendVideoFrames(frame: sampleBuffer)
+            
+            
+//            var pointer: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>?
+//            guard let blockBuffer = CMSampleBufferGetDataBuffer(sampleBuffer) else {
+//                print("Error block buffer")
+//                return
+//            }
+//
+//            let frames = CMBlockBufferGetDataPointer(blockBuffer,
+//                                                     atOffset: 0,
+//                                                     lengthAtOffsetOut: nil,
+//                                                     totalLengthOut: nil,
+//                                                     dataPointerOut: pointer)
+           
+//            TCPClient.shared.send()
+        }
+    }
 }
 
